494c2579d2eb85a5385b0f82bc1e610bc4aa6e45
HarryCCC <1147061986@qq.com>
2023-12-13 02:11:05 +0800

    脚本同步

--- sync.sh
+++ sync.sh
@@ -19,13 +19,9 @@ git commit -m "脚本同步"
 # 添加远程仓库（如果已经存在，此命令不会有任何效果）
 git remote add origin "$REMOTE_URL" 2> /dev/null || git remote set-url origin "$REMOTE_URL"
 
-# 获取远程主分支的最新更改
-git pull origin main --rebase
-
-# 推送到远程仓库的主分支
-git push origin main
+# 强制推送到远程仓库的主分支，覆盖云端的所有更改
+git push -f origin main
 
 # 放弃合并:   git rebase --abort
 # 初始配置：  git config --global user.name "Your Name"      git config --global user.email "your@email.com"
 # 脚本赋权:   chmod +x sync.sh
-
